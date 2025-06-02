1e51ddc74035b77f5fc5e25be34a36fa53bbfd8a
emilyw03 <emilywang9379@gmail.com>
2025-06-02 18:11:08 -0400

    csv merging script

--- csv_merging.py
+++ csv_merging.py
@@ -14,6 +14,7 @@ csv_files = sorted(glob.glob("2cof_float3_interval_*_20250602.csv"))
 
 # Load and concatenate all CSVs
 df_all = pd.concat((pd.read_csv(f) for f in csv_files), ignore_index=True)
+df_all = df_all.sort_values(by='alpha', ascending=True)
 
 # Save to a single merged file
 df_all.to_csv("2cof_float3_interval_20250602.csv", index=False)
