37b9ae99fbdbd14bb9e248cf83a9ab897b14879b
emilyw03 <emilywang9379@gmail.com>
2025-06-10 10:37:21 -0400

    merged alpha = 1, whole square files

--- csv_merging.py
+++ csv_merging.py
@@ -10,12 +10,18 @@ import pandas as pd
 import glob
 
 # Find all matching CSV files
-csv_files = sorted(glob.glob("2cof_float3_interval_*_20250602.csv"))
+csv_files = sorted(glob.glob("BestBump_alpha1_*_20250609.csv"))
 
 # Load and concatenate all CSVs
 df_all = pd.concat((pd.read_csv(f) for f in csv_files), ignore_index=True)
-df_all = df_all.sort_values(by='alpha', ascending=True)
+df_all = df_all.sort_values(by='slopesL', ascending=True)
+df_all = df_all.rename(columns={
+    'slopesL': 'slopeL',
+    'F_t': 'F',
+    'F_slip_best': 'F_slip',
+    'F_yield_best': 'F_yield'
+})
 
 # Save to a single merged file
-df_all.to_csv("2cof_float3_interval_20250603.csv", index=False)
+df_all.to_csv("BestBump_alpha1_20250609.csv", index=False)
 print("csv merged")
