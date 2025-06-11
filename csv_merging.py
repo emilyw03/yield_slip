3eb888d0fbaef381dfe7d2f3d4d9a4e327b3bc31
emilyw03 <emilywang9379@gmail.com>
2025-06-11 16:45:46 -0400

    line for adding new column to csv

--- csv_merging.py
+++ csv_merging.py
@@ -9,6 +9,7 @@ Use for merging csv's after parallelized slurm tasks
 import pandas as pd
 import glob
 
+'''
 # Find all matching CSV files
 csv_files = sorted(glob.glob("BestBump_alphapt03_*_20250610.csv"))
 
@@ -19,3 +20,8 @@ df_all = df_all.sort_values(by='slopeL', ascending=True)
 # Save to a single merged file
 df_all.to_csv("BestBump_alphapt03_20250610.csv", index=False)
 print("csv merged")
+'''
+
+ramps = pd.read_csv("ramps2_grid_300_20250415.csv")
+ramps["potential_H1"] = 0.300 + ramps["slopeH"]
+ramps.to_csv("ramps2_grid_300_20250415.csv", index=False)
