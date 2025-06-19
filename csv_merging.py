da67fa87c0851c736dfb29702fd680cdff6e51b9
emilyw03 <emilywang9379@gmail.com>
2025-06-19 17:03:00 -0400

    merge new Nfn1 vary slopeH data (added short circuit fluxes)

--- csv_merging.py
+++ csv_merging.py
@@ -10,18 +10,18 @@ import pandas as pd
 import numpy as np
 import glob
 
-'''
+
 # === merged parallelized files ===
 # Find all matching CSV files
-csv_files = sorted(glob.glob("Nfn1_vary_slopeH_ramp_*_20250618.csv"))
+csv_files = sorted(glob.glob("Nfn1_vary_slopeH_bump_*_20250619.csv"))
 
 # Load and concatenate all CSVs
 df_all = pd.concat((pd.read_csv(f) for f in csv_files), ignore_index=True)
 df_all = df_all.sort_values(by='slopeH', ascending=True)
 
 # Save to a single merged file
-df_all.to_csv("Nfn1_vary_slopeH_ramp_20250618.csv", index=False)
-print("csv merged")'''
+df_all.to_csv("Nfn1_vary_slopeH_bump_20250619.csv", index=False)
+print("csv merged")
 
 '''
 # === add dG ===
@@ -41,7 +41,7 @@ df['dG'] = dG
 df.to_csv("BestBump_alpha1_corner_20250611.csv", index=False)
 '''
 
-
+'''
 # === filter for bifurcating only (dG <= 0)
 df = pd.read_csv("Nfn1_vary_slopeH_bump_20250618.csv")
 filtered = pd.read_csv("Nfn1_vary_slopeH_bump_nonbif_20250618.csv")
@@ -60,5 +60,5 @@ filtered["slopeH_rounded"] = filtered["slopeH"].round(6)
 filtered_only = filtered[~filtered["slopeH_rounded"].isin(df["slopeH_rounded"])]
 
 # Save to CSV
-filtered_only.to_csv("Nfn1_vary_slopeH_bump_nonbif_test_20250618.csv", index=False)
+filtered_only.to_csv("Nfn1_vary_slopeH_bump_nonbif_test_20250618.csv", index=False)'''
 
