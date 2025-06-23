a0917ce105b9ed62c1e9b2266d4be69d1d88940a
emilyw03 <emilywang9379@gmail.com>
2025-06-23 11:56:22 -0400

    Filter updated Nfn1 data for bifurcating only

--- csv_merging.py
+++ csv_merging.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import glob
 
-
+'''
 # === merged parallelized files ===
 # Find all matching CSV files
 csv_files = sorted(glob.glob("Nfn1_varyL_ramp_*_20250620.csv"))
@@ -21,7 +21,7 @@ df_all = df_all.sort_values(by='slopeL', ascending=True)
 
 # Save to a single merged file
 df_all.to_csv("Nfn1_varyL_ramp_all_20250620.csv", index=False)
-print("csv merged")
+print("csv merged")'''
 
 '''
 # === add dG ===
@@ -41,14 +41,14 @@ df['dG'] = dG
 df.to_csv("BestBump_alpha1_corner_20250618.csv", index=False)
 '''
 
-'''
+
 # === filter for bifurcating only (dG <= 0)
-df = pd.read_csv("BestBump_alpha1_corner_20250618.csv")
+df = pd.read_csv("Nfn1_varyL_ramp_all_20250620.csv")
 #filtered = pd.read_csv("Nfn1_vary_slopeH_ramp_nonbif_20250618.csv")
-filtered = df[(df['dG'] <= 0) & (df['fluxD'] < 0) & (df['fluxHR'] > 0) & (df['fluxLR'] > 0)]
-#filtered = df[(df['NADPH_flux'] < 0) & (df['NAD_flux'] > 0) & (df['Fd_flux'] > 0) & (df['Fd_flux'] <= df['NAD_flux'])]
-filtered.to_csv("BestBump_alpha1_corner_bif_20250618.csv", index=False)
-'''
+#filtered = df[(df['dG'] <= 0) & (df['fluxD'] < 0) & (df['fluxHR'] > 0) & (df['fluxLR'] > 0)]
+filtered = df[(df['NADPH_flux'] < 0) & (df['NAD_flux'] > 0) & (df['Fd_flux'] > 0) & (df['Fd_flux'] <= df['NAD_flux'])]
+filtered.to_csv("Nfn1_varyL_ramp_bif_20250620.csv", index=False)
+
 '''
 # find non-bifurcating in bifurcating range
 df = df.copy()
