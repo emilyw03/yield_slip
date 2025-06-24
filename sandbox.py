0dce32a6e017e78269b8d2fb2fc0332a324790da
emilyw03 <emilywang9379@gmail.com>
2025-06-24 17:46:17 -0400

    sync changes

--- sandbox.py
+++ sandbox.py
@@ -3,24 +3,14 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-bump_0 = pd.read_csv('BestBump_alpha0_20250606.csv')
-bump_1 = pd.read_csv('BestBump_alpha1_20250609.csv')
+df = pd.read_csv('Nfn1_bump_size_20250623.csv')
+plt.scatter(df['pH1'], df['F_slip'], s=20)
+plt.title('F_slip vs. potential on H1', fontsize=10)
+plt.xlabel('Potential on H1 (eV)')
+plt.ylabel('F_slip')
+plt.tight_layout()
+plt.legend()
+plt.show()
 
-sorted_y = bump_1['F_yield'].sort_values()
-sorted_s = bump_1['F_slip'].sort_values()
-
-print('F_yield: min, max - 1, max:', sorted_y.iloc[0], sorted_y.iloc[-2], sorted_y.iloc[-1])
-print('F_slip: min, max - 1, max:', sorted_s.iloc[0], sorted_s.iloc[-2], sorted_s.iloc[-1])
-
-F_yield_0 = bump_0["F_yield"]
-F_yield_0 = F_yield_0[F_yield_0 != F_yield_0.max()]
-
-F_yield_1 = bump_1["F_yield"]
-F_yield_1 = F_yield_1[F_yield_1 != F_yield_1.max()]
-
-F_slip_diff = bump_1['F_slip'] - bump_0['F_slip']
-F_yield_diff = F_yield_1 - F_yield_0
-
-print('F_slip diff (min, max): ', min(F_slip_diff), max(F_slip_diff))
-print('F_yield diff (min, max): ', min(F_yield_diff), max(F_yield_diff))
