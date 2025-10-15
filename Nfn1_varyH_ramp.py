c6f5811c3f0acc62231ba017ab9e2b6086aca31c
emilyw03 <emilywang9379@gmail.com>
2025-10-15 11:57:47 -0400

    update ramp varyH script

--- Nfn1_varyH_ramp.py
+++ Nfn1_varyH_ramp.py
@@ -132,10 +132,11 @@ if __name__ == '__main__':
     results = []
     time = ztime*(10**(N*dt))
     for val in chunk:
-        NADPH, NAD, Fd, F_slip, F_yield, D_to_H1_flux, L1_to_D_flux = Nfn1(-0.118, val, time)
+        mu_FeS_H1 = 0.04 + slopeH
+        NADPH, NAD, Fd, F_slip, F_yield, D_to_H1_flux, L1_to_D_flux = Nfn1(mu_FeS_H1, val, time)
         S_FAD_mid = 0.040 + 2*val
         results.append([val, NADPH, NAD, Fd, F_slip, F_yield, D_to_H1_flux, L1_to_D_flux, S_FAD_mid])
 
     columns = ["slopeH", "NADPH_flux", "NAD_flux", "Fd_flux", "F_slip", "F_yield", "D_to_H1_flux", "L1_to_D_flux", "S_FAD_mid_pot"]
     df = pd.DataFrame(results, columns=columns)
-    df.to_csv(f"Nfn1_vary_slopeH_ramp_{task_id}_20250620.csv", index=False)
+    df.to_csv(f"Nfn1_vary_slopeH_ramp_{task_id}_20251015.csv", index=False)
