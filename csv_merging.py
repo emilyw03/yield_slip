ae32d20dbf4f817736fd67835f4567fc8696d50a
Emily Wang <emilywang9379@gmail.com>
2025-10-15 12:52:05 -0400

    for merging new slopeH files

--- csv_merging.py
+++ csv_merging.py
@@ -11,24 +11,24 @@ import numpy as np
 import glob
 
 
-'''
+
 # === merged parallelized files ===
 # Find all matching CSV files
-csv_files = sorted(glob.glob("Nfn1_varyH_ramp_fixDist_*_20250711.csv"))
+csv_files = sorted(glob.glob("Nfn1_varyH_ramp_*_20251015.csv"))
 
 # Load and concatenate all CSVs
 df_all = pd.concat((pd.read_csv(f) for f in csv_files), ignore_index=True)
 df_all = df_all.sort_values(by=['slopeH'], ascending=True)
 
 # Save to a single merged file
-df_all.to_csv("Nfn1_varyH_ramp_fixDist_20250711.csv", index=False)
+df_all.to_csv("Nfn1_varyH_ramp_20251015.csv", index=False)
 print("csv merged")
+
+
 '''
-
-
 # === filter for bifurcating only fluxD < 0, fluxH, fluxL > 0
 df = pd.read_csv("bump_Fyield_20250629.csv")
 filtered = df[(df['fluxD'] < 0) & (df['fluxHR'] > 0) & (df['fluxLR'] > 0)]
 #filtered = df[(df['NADPH_flux'] < 0) & (df['NAD_flux'] > 0) & (df['Fd_flux'] > 0)]
 filtered.to_csv("bump_Fyield_bif_20250629.csv", index=False)
-
+'''
